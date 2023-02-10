c842ebeb749b4fae81a3604b51ad3479430b6b2e
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-02-10 23:53:57 +0530

    precedence and associativity

--- Operator_1.java
+++ Operator_1.java
@@ -1,3 +1,5 @@
+//- Write a program to add 8 to the number x and then divide it by 3. Now, the modulus of the quotient
+ //is taken with 5 and then multiply the resultant value by 5. Display the final result
 import java.util.Scanner;
 public class Operator_1 {
     public static void main(String[] args) {
