74809328ee5e7db9311bea6e8a076a853a6b4903
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-02-10 23:51:27 +0530

    precedence and associativity

--- SumOfDigits.java
+++ SumOfDigits.java
@@ -2,14 +2,14 @@ import java.util.*;
 public class SumOfDigits {
     public static void main(String[] args) {
         Scanner sc =new Scanner(System.in);
-        System.out.print("Enter any digit number: ");
+        System.out.print("Enter any digits number: ");
         int x=sc.nextInt();
         int sum =0;
         while(x>0){
-            sum +=x%10;
+            sum +=x%10;// to calculate the sum of the digits .
             x/=10;
         }
-        System.out.println("sum of all digit number is : "+sum);
+        System.out.println("sum of all digits number is : "+sum);
     }
     
 }
