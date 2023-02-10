5746cbc98a270618fa526692f477c8315a6ea5f0
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-02-10 23:54:58 +0530

    Conditional

--- Absolute.java
+++ Absolute.java
@@ -4,7 +4,7 @@ public class Absolute {
         Scanner sc =new Scanner(System.in);
         System.out.print("Enter the number : ");
         int n=sc.nextInt();
-        if(n<0){
+        if(n<0){// to print absolute value of a number entered by the user.
             n *=-1;
         }
         System.out.println("The Absolute number is : "+n  );
--- Conditional.java
+++ Conditional.java
@@ -4,6 +4,7 @@ public class Conditional{
         Scanner sc =new Scanner(System.in);
         System.out.print("Enter the number : ");
         int n =sc.nextInt();
+        //to print positive number entered by the user, if user enters a negative number, it is skipped.
         if(n>=0){
             System.out.println("You  entered postive number i.e : "+n);
         }else{
