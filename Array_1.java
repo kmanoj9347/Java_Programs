679360aad6e68c867ed4f347d8d784cde8d92982
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-02-11 23:51:45 +0530

    Array

---
+++ Array_1.java
@@ -0,0 +1,10 @@
+public class Array_1 {
+    public static void main(String args[]){
+        int[] arr={-3,3,-2,2,-1,1,0};
+         for(int val : arr ){
+            if(val >0){ 
+            System.out.println(val);
+        }
+    }
+}
+}
