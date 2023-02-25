d16b080ffbf50160a21303a6a96d68d5ed08b649
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-02-25 17:55:06 +0530

    Array

---
+++ Array_2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Array_2 {
+    public static void main(String[] args) {
+        String[] arr ={"ab","bc","cd","de","ef","fg","gh"};
+        for(int i=0;i <arr.length;i++){
+        if(i % 2 !=0){
+        System.out.println(arr[i]);
+       }
+    }   
+  }
+}
