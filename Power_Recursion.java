778e59ed8dd5c48cb0138c98b1f21cdc065ab55f
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-03-01 09:10:58 +0530

    Recursion

---
+++ Power_Recursion.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Power_Recursion {
+    public static int func(int a ,int b){ 
+        if(b==0)
+        return 1;
+        return(a*(func(a,b-1)));
+    }
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int a=sc.nextInt();
+        System.out.print("Enter the power number : ");
+        int b=sc.nextInt();
+        System.out.println("Required power is : "+func(a,b));
+    }
+}
