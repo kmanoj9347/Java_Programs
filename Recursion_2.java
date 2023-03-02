2f6258157a0855cb34c6e8e1945928e821c27032
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-03-02 11:58:34 +0530

    Recursion

---
+++ Recursion_2.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+public class Recursion_2 {
+    public static void func(int n ,int k,int i){
+        if(k==0)
+        return ;
+        System.out.println((n*i)+" "); 
+        func(n,k-1,i+1);
+    }
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int n=sc.nextInt();
+        System.out.print("Enter the k value : ");
+        int k=sc.nextInt();
+        func(n,k,1);
+    }
+}
