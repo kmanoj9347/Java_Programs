9798051015f4461179fedb89ef5019319bebf542
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-03-06 23:12:35 +0530

    Recursion

--- Recursion_6.java
+++ Recursion_6.java
@@ -1,25 +1,20 @@
 import java.util.*;
-import java.io.*;
 public class Recursion_6 {
-    static Boolean Exists(int a[],int n,int tgt ,int idx){
-        if(idx >=n )return false ;
-        if (a[idx] == tgt)return true;
-        return Exists(a, n ,tgt ,idx+1);
-    } 
-    public static void main(String[] args) {
-        int n,tgt;
-        Scanner sc =new Scanner(System.in);
-        n =sc.nextInt();
-        System.out.println("Enter the array elements : ");
-        int a[] =new int[n];
-        for(int i =0;i <n;i++){
-            a[i]=sc.nextInt();
+    static void printNDigitNumber(int start,String out,int n){
+        if(n == 0){
+            System.out.print(out +" ");
+            return;
         }
-        tgt =sc.nextInt();
-        if(Exists(a,n,tgt,0))
-        System.out.println("yes");
-        else 
-        System.out.println("no");
+        for(int i =start;i <=9;i++){
+            String str =out +Integer.toString(i);
+            printNDigitNumber(i +1,str,n-1);
+        }
     }
+   public static void main(String[] args) {
+    Scanner sc =new Scanner(System.in);
+    System.out.println("Enter the number : ");
+    int n=sc.nextInt();
+    printNDigitNumber(0," ",n);
+    
+   } 
 }
-
