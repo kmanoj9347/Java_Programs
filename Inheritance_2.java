5ae9ec9710e61e911002b80f39ec883337c4cf78
Manoj Kumar <khamitkarmanoj@gmail.com>
2023-03-12 15:33:29 +0530

    Multilevel_Inheritance

---
+++ Inheritance_2.java
@@ -0,0 +1,27 @@
+//Multilevel inheritance
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+class one{
+    public void print_hai(){
+        System.out.println("Hai");
+    }
+}
+class two extends one {
+    public void print_hello(){
+        System.out.println("Hello");
+    }
+}
+class three extends two {
+    public void print_bye() {
+        System.out.println("Bye");
+    }
+}
+public class Inheritance_2 {
+    public static void main(String[] args) {
+        three obj =new three();
+        obj.print_hai();
+        obj.print_hello();
+        obj.print_bye();
+    }
+}
